bc247b1e30acbab1465ab627c9c256fd754d0969
jeonjyeon <peacheese17@gmail.com>
2025-08-12 14:34:56 +0900

    learn: 문 vs. 식
    - 문은 값이 없다.
    - 식은 값이 있다.
    - 문은 함수 몸체에서만 사용 가능
    - 식은 함수 몸체 또는 React.createElement() 내부에서 모두 사용 가능

--- 03-react-component/src/app.js
+++ 03-react-component/src/app.js
@@ -5,20 +5,38 @@ import { OutputFunction as Output } from "./components/output.js";
 export function App(props) {
   // console.log(props) /* { count, targetCount } */
 
-  let isAnimate = true;
+  // let isAnimate = true;
 
   // 조건부 렌더링 (Conditional Rendering)
   // 애니메이션이 종료되면 [ 위글 애니메이션도 ] 멈춘다.
-  if (props.count >= props.targetCount) {
-    console.log("애니메이션 종료!");
-    isAnimate = false;
-  }
+
+  // 문 (값이 없다)
+  // if (props.count >= props.targetCount) {
+  //   console.log("애니메이션 종료!");
+  //   isAnimate = false;
+  // }
+
+  // createElement() 함수 내부에서는 문을 사용할 수 없다.
+  // 왜? 값이 없으니까! 문 대신, 식을 사용해야 한다.
+
+  // 식 (값이 있다)
+  // const isComplete = props.count >= props.targetCount
+  // 논리 연산자 식
+  // 삼항 연산자 식
+  // 옵셔널 체이닝(?.), null 병합 연산자(??)
 
   const app = React.createElement(
     "div",
     { className: "randomCountUpApp" },
     React.createElement(Logo, {}),
-    React.createElement(Output, { isAnimate }, props.count),
+    // React.createElement(Output, { isAnimate }, props.count),
+    React.createElement(
+      Output,
+      {
+        isAnimate: props.count < props.targetCount ? true : false,
+      },
+      props.count
+    ),
     React.createElement(Shortcut, {})
   );
   return app;
