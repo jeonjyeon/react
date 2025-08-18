09123d05eecabdc1025718baf251575ae5ee36ad
jeonjyeon <peacheese17@gmail.com>
2025-08-19 02:52:28 +0900

    feat: 같은 아코디언 아이템 클릭 시 열림/닫힘 토글 로직 구현

--- 10-stateful-component/src/components/accordion/accordion-list.jsx
+++ 10-stateful-component/src/components/accordion/accordion-list.jsx
@@ -7,15 +7,34 @@ export default class AccordionList extends Component {
   // 상태 선언
   // 클래스 필드 구문 활용
   state = {
-    activeIndex: 0,
+    activeIndex: null,
   }
 
   // 이벤트 핸들러
+  // handleActiveIndex = (nextActiveIndex) => {
+  //   // 상태 업데이트
+  //   this.setState({ activeIndex: nextActiveIndex }, () => {
+  //     console.log(this.state.activeIndex)
+  //   })
+  // }
+
+  /**
+   * 아코디언 아이템 클릭 시 실행되는 핸들러
+   * 부모(AccordionList)가 자식(AccordionItem)에게 핸들러(onActive)를 전달하고,
+   * 자식이 자신의 index를 인자로 넘겨 호출함
+   * @param {number} nextActiveIndex - 클릭된 아이템의 인덱스 값
+   * @description
+   * - 같은 인덱스를 다시 클릭하면 닫힘(null)
+   * - 다른 인덱스를 클릭하면 해당 인덱스로 열림
+   * - prevState는 상태 변경 직전의 state 값
+   */
   handleActiveIndex = (nextActiveIndex) => {
     // 상태 업데이트
-    this.setState({ activeIndex: nextActiveIndex }, () => {
-      console.log(this.state.activeIndex)
-    })
+    this.setState((prevState) => ({
+      // 같은 인덱스가 클릭되면 아코디언 아이템 닫힘 처리
+      activeIndex:
+        prevState.activeIndex === nextActiveIndex ? null : nextActiveIndex,
+    }))
   }
 
   render() {
