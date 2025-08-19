ac7d780b149791b04b563d1aaca5d590a73bf130
jeonjyeon <peacheese17@gmail.com>
2025-08-19 11:12:41 +0900

    learn: 함수형 컴포넌트의 상태 관리
    - useState 훅
    - useId 훅

--- 10-stateful-component/src/app.jsx
+++ 10-stateful-component/src/app.jsx
@@ -2,12 +2,13 @@ import { LearnSection } from '@/components'
 import AccordionListClass from './components/accordion/accordion-list'
 import StatefulComponentClass from './components/stateful-component/class'
 import StatefulComponent from './components/stateful-component/functional'
+import UpdateInput from './components/update-input'
 
 export default function App() {
   return (
     <LearnSection title="상태 있는/없는 컴포넌트 구성" showTitle={false}>
-      <AccordionListClass onlyOneOpen />
-      <AccordionListClass />
+      <UpdateInput label="이름" />
+      <UpdateInput label="직업" />
     </LearnSection>
   )
 }
@@ -15,6 +16,15 @@ export default function App() {
 // --------------------------------------------------------------------------
 // 실습 데모
 // --------------------------------------------------------------------------
+
+function AccordionListClassDemo() {
+  return (
+    <LearnSection title="상태 있는/없는 컴포넌트 구성" showTitle={false}>
+      <AccordionListClass onlyOneOpen />
+      <AccordionListClass />
+    </LearnSection>
+  )
+}
 
 function DescriptionComponentTypes() {
   return (
---
+++ 10-stateful-component/src/components/update-input/index.jsx
@@ -0,0 +1,48 @@
+import { useId, useState } from 'react'
+import './style.css'
+
+/**
+ * UpdateInput 컴포넌트
+ * @param {Object} props
+ * @param {string} props.label - 인풋 요소의 레이블
+ */
+export default function UpdateInput({ label }) {
+  console.log('UpdateInput 컴포넌트 렌더링')
+
+  // 리액트 훅 함수를 사용해 함수형 컴포넌트의 상태 관리
+  // const stateAdapter = useState('지연')
+  // console.log(stateAdapter.at(0)) // 현재 상태 값
+  // console.log(stateAdapter.at(1)) // 상태 업데이트 함수
+
+  const [username, setUsername] = useState('') // [state, setState]
+  const id = useId()
+
+  // 상태 선언
+  // const name = '지연'
+
+  // 상태 업데이트
+  // const setName = () => {}
+
+  // 이벤트 핸들러
+  const handleInput = (e) => {
+    // console.log(e.target.value)
+    const nextUsername = e.target.value
+    setUsername(nextUsername) // 상태 업데이트
+  }
+
+  return (
+    <>
+      <div className="update-input">
+        <label htmlFor={id}>{label}</label>
+        {/* uncontrolled component */}
+        <input
+          id={id}
+          type="text"
+          defaultValue={username}
+          onInput={handleInput}
+        />
+      </div>
+      <output>{username}</output>
+    </>
+  )
+}
