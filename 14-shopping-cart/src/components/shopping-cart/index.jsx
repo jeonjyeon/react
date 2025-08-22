8e0aeeb26ae5aae8695d456440b58507494ebd31
jeonjyeon <peacheese17@gmail.com>
2025-08-23 03:20:35 +0900

    refactor: 상품별 수량 조절 함수(updateProductQuantity)에 useImmer 적용

--- 14-shopping-cart/src/components/shopping-cart/index.jsx
+++ 14-shopping-cart/src/components/shopping-cart/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useImmer } from 'use-immer'
 import INITIAL_PRODUCTS from '@/data/products.json'
 import CartFooter from './cart-footer'
 import CartHeader from './cart-header'
@@ -10,7 +11,8 @@ import CartList from './cart-list'
  */
 export default function ShoppingCart() {
   // 상품 갯수
-  const [products, setProducts] = useState(INITIAL_PRODUCTS)
+  const [products, setProducts] = useImmer(INITIAL_PRODUCTS)
+  // const [products, setProducts] = useState(INITIAL_PRODUCTS)
 
   // 총 가격
   const totalPrice = products.reduce(
@@ -23,15 +25,26 @@ export default function ShoppingCart() {
    * @param {number|string} productId - 상품 id
    * @param {number} amount - 변경할 수량(양수: 증가, 음수: 감소)
    */
+  // useImmer 사용 코드
   const updateProductQuantity = (productId, amount) => {
-    setProducts((prevProducts) =>
-      prevProducts.map((product) =>
-        product.id === productId
-          ? { ...product, quantity: product.quantity + amount }
-          : product
-      )
-    )
+    setProducts((draft) => {
+      const product = draft.find((p) => p.id === productId)
+      if (product) {
+        product.quantity += amount
+      }
+    })
   }
+
+  // useState 사용 코드
+  // const updateProductQuantity = (productId, amount) => {
+  //   setProducts((prevProducts) =>
+  //     prevProducts.map((product) =>
+  //       product.id === productId
+  //         ? { ...product, quantity: product.quantity + amount }
+  //         : product
+  //     )
+  //   )
+  // }
 
   return (
     <section className="m-4 flex max-w-[692px] min-w-[320px] flex-col justify-between items-center p-5  border-[color:var(--gray-700,#4d4d4d)] rounded-lg border-[6px] border-solid">
