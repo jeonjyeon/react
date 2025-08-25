8e6c117a8e16699f38c7e33f8f3f43527a71fef8
jeonjyeon <peacheese17@gmail.com>
2025-08-25 09:32:31 +0900

    add: 가상 DOM의 작동 방식 준비

---
+++ 15-virtual-dom/src/app.jsx
@@ -0,0 +1,9 @@
+import { LearnSection } from '@/components'
+
+export default function App() {
+  return (
+    <LearnSection title="가상 DOM의 작동 방식" showTitle={false}>
+      <div>가상 DOM의 작동 방식</div>
+    </LearnSection>
+  )
+}
