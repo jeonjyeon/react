1249e88f7f809a7e6eee23ae1ae8aa8e128b49e3
jeonjyeon <peacheese17@gmail.com>
2025-08-25 11:14:26 +0900

    add: 컴포넌트 라이프사이클 준비

---
+++ 16-component-lifecycle/src/app.jsx
@@ -0,0 +1,9 @@
+import { LearnSection } from '@/components'
+
+export default function App() {
+  return (
+    <LearnSection title="컴포넌트 라이프사이클">
+      <h2>컴포넌트 라이프사이클</h2>
+    </LearnSection>
+  )
+}
