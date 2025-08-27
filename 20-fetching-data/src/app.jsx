3535cac3beeb5e3dac89043b79723fd564d51e50
jeonjyeon <peacheese17@gmail.com>
2025-08-27 11:02:34 +0900

    learn. 데이터 가져오기
    - 부수 효과 코드가 렌더링 영역에 포함된 경우

--- 20-fetching-data/src/app.jsx
+++ 20-fetching-data/src/app.jsx
@@ -1,10 +1,18 @@
 import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo/app'
 
 export default function App() {
+  console.log('App 렌더링')
+
+  // 리액트 렌더링(동기 방식으로 작동)과
+  // 무관한 서버에서 데이터 가져오기 코드
+  fetch('https://jsonplaceholder.typicode.com/albums/1')
+    .then((response) => response.json())
+    .then(console.log)
+    .catch(console.error)
+
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
+    <LearnSection title="데이터 가져오기(fetching data)" showTitle>
+      <p>{'앨범 이름'}</p>
     </LearnSection>
   )
 }
