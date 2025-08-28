76642e4d9199f98a03ff7f1add960d4e777ee11c
jeonjyeon <peacheese17@gmail.com>
2025-08-28 16:00:46 +0900

    learn: 틱택토 게임 구현 1

--- 24-tic-tac-toe/src/components/tic-tac-toe/index.jsx
+++ 24-tic-tac-toe/src/components/tic-tac-toe/index.jsx
@@ -1,116 +1,102 @@
+import { useState } from 'react'
+
 export default function TicTacToe() {
   return (
     <div className="Game">
-      <div className="Board">
-        <h2 className="Status" role="status">
-          다음 플레이어 🟨
-        </h2>
-        <div
-          className="Squares"
-          role="grid"
-          aria-label="틱택토 게임판"
-          aria-rowcount={3}
-          aria-colcount={3}
-        >
+      <Board />
+      <History />
+    </div>
+  )
+}
+
+// 게임 상수
+const PLAYER = {
+  ONE: '⚫️',
+  TWO: '🟨',
+}
+
+const INITIAL_SQUARES = Array(9).fill(null)
+
+function Board() {
+  return (
+    <div className="Board">
+      <Status />
+      <Squares />
+    </div>
+  )
+}
+
+function Status() {
+  return (
+    <h2 className="Status" role="status">
+      다음 플레이어 🟨
+    </h2>
+  )
+}
+
+function Squares() {
+  const squares = [...INITIAL_SQUARES]
+
+  return (
+    <div
+      className="Squares"
+      role="grid"
+      aria-label="틱택토 게임판"
+      aria-rowcount={3}
+      aria-colcount={3}
+    >
+      {squares.map((square, index) => {
+        return <SquareButton key={index}>{index}</SquareButton>
+      })}
+    </div>
+  )
+}
+
+function SquareButton({ children }) {
+  return (
+    <button
+      role="gridcell"
+      aria-rowindex={1}
+      aria-colindex={1}
+      className="Square"
+      aria-label="첫 번째 칸, 비어 있음"
+      disabled={false}
+    >
+      {children}
+    </button>
+  )
+}
+
+// --------------------------------------------------------------------------
+function History() {
+  return (
+    <div className="History">
+      <ol className="HistoryList">
+        <li className="HistoryListItem">
+          <button type="button" className="HistoryButton">
+            게임 시작!
+          </button>
+        </li>
+        <li className="HistoryListItem">
           <button
-            role="gridcell"
-            aria-rowindex={1}
-            aria-colindex={1}
-            className="Square"
-            aria-label="첫 번째 칸, 🟨"
+            type="button"
+            className="HistoryButton"
+            aria-label="게임 #1 이동"
+          >
+            게임 #1
+          </button>
+        </li>
+        <li className="HistoryListItem">
+          <button
+            type="button"
+            className="HistoryButton"
+            aria-label="게임 #2 이동"
             disabled
           >
-            🟨
+            게임 #2
           </button>
-          <button
-            role="gridcell"
-            aria-rowindex={1}
-            aria-colindex={2}
-            className="Square"
-            aria-label="두 번째 칸, 비어 있음"
-          />
-          <button
-            role="gridcell"
-            aria-rowindex={1}
-            aria-colindex={3}
-            className="Square"
-            aria-label="세 번째 칸, 비어 있음"
-          />
-          <button
-            role="gridcell"
-            aria-rowindex={2}
-            aria-colindex={1}
-            className="Square"
-            aria-label="네 번째 칸, 비어 있음"
-          />
-          <button
-            role="gridcell"
-            aria-rowindex={2}
-            aria-colindex={2}
-            className="Square"
-            aria-label="다섯 번째 칸, ⚫️"
-            disabled
-          >
-            ⚫️
-          </button>
-          <button
-            role="gridcell"
-            aria-rowindex={2}
-            aria-colindex={3}
-            className="Square"
-            aria-label="여섯 번째 칸, 비어 있음"
-          />
-          <button
-            role="gridcell"
-            aria-rowindex={3}
-            aria-colindex={1}
-            className="Square"
-            aria-label="일곱 번째 칸, 비어 있음"
-          />
-          <button
-            role="gridcell"
-            aria-rowindex={3}
-            aria-colindex={2}
-            className="Square"
-            aria-label="여덟 번째 칸, 비어 있음"
-          />
-          <button
-            role="gridcell"
-            aria-rowindex={3}
-            aria-colindex={3}
-            className="Square"
-            aria-label="아홉 번째 칸, 비어 있음"
-          />
-        </div>
-      </div>
-      <div className="History">
-        <ol className="HistoryList">
-          <li className="HistoryListItem">
-            <button type="button" className="HistoryButton">
-              게임 시작!
-            </button>
-          </li>
-          <li className="HistoryListItem">
-            <button
-              type="button"
-              className="HistoryButton"
-              aria-label="게임 #1 이동"
-            >
-              게임 #1
-            </button>
-          </li>
-          <li className="HistoryListItem">
-            <button
-              type="button"
-              className="HistoryButton"
-              aria-label="게임 #2 이동"
-              disabled
-            >
-              게임 #2
-            </button>
-          </li>
-        </ol>
-      </div>
+        </li>
+      </ol>
     </div>
   )
 }
